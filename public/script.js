56f2080cf146902bc864c65c6dfabdf915848b87
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-03-30 10:48:31 -0300

    Added photo upload and styled

--- public/script.js
+++ public/script.js
@@ -10,3 +10,18 @@ const Mask = {
     return value = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value / 100)
   }
 };
+
+const PhotosUpload = {
+  uploadLimiti: 6,
+  handleFileInput(event) {
+    const { files: fileList } = event.target;
+    const { uploadLimiti } = PhotosUpload
+
+
+    if (fileList.length > uploadLimiti) {
+      alert(`Envie no máximo ${uploadLimiti} fotos.`)
+      event.preventDefault();
+      return
+    }
+  }
+}

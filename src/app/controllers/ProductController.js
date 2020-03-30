2727e347c4fa524deaa1949525684759da3ca7f3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-03-30 10:15:46 -0300

    add files

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,74 @@
+const { formatPrice } = require('../../lib/utils');
+
+const Category = require('../models/Category');
+const Product = require('../models/Product');
+
+module.exports = {
+  create(req, res) {
+    Category.all().then(function(results) {
+      const categories = results.rows;
+
+      return res.render('products/create.njk', { categories });
+    }).catch(function(err) {
+      throw new Error(err);
+    })
+  },
+
+  async post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == '') {
+        return res.send('Por favor, preencha todos os campos.')
+      }
+    }
+
+    const results = await Product.create(req.body)
+    const productId = results.rows[0].id
+
+    return res.redirect(`/products/${productId}/edit`)
+  },
+
+  async edit(req, res) {
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if(!product) return res.send('Produto não encontrado.')
+
+    product.price = formatPrice(product.price);
+    product.old_price = formatPrice(product.old_price);
+
+    results = await Category.all();
+    const categories = results.rows;
+
+    return res.render('products/edit.njk', { product, categories })
+  },
+
+  async put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == '') {
+        return res.send('Por favor, preencha todos os campos.')
+      }
+    }
+
+    req.body.price = req.body.price.replace(/\D/g,'');
+
+    if(req.body.old_price != req.body.price) {
+      const oldPrice = await Product.find(req.body.id);
+
+      req.body.old_price = oldPrice.rows[0].price;
+    }
+
+    await Product.update(req.body);
+
+    return res.redirect(`/products/${req.body.id}/edit`);
+  },
+
+  async delete(req, res) {
+    await Product.delete(req.body.id);
+
+    return res.redirect('products/create');
+  }
+}
---
+++ src/lib/utils.js
@@ -0,0 +1,25 @@
+module.exports = {
+  date(timestamp) {
+    const date = new Date(timestamp);
+
+    const year = date.getUTCFullYear();
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+    const day = `0${date.getUTCDate()}`.slice(-2);
+
+    return {
+      day,
+      month,
+      year,
+      format: `${day}/${month}/${year}`,
+      birthDay: `${day}/${month}`,
+      iso:`${year}-${month}-${day}`,
+    }
+  },
+
+  formatPrice(price) {
+    return new Intl.NumberFormat('pt-BR', { 
+      style: 'currency', 
+      currency: 'BRL' 
+    }).format(price / 100)
+  }
+};

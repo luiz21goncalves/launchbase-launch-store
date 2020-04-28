66b26308fb674a1db971ea55790582b5c19cf6f8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-04-28 14:34:03 -0300

    add users table

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS launchstoredb;
+CREATE DATABASE launchstoredb;
+
 CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int NOT NULL,
@@ -8,6 +11,18 @@ CREATE TABLE "products" (
   "price" int NOT NULL,
   "quantity" int DEFAULT 0,
   "status" int DEFAULT 1,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
@@ -27,3 +42,23 @@ CREATE TABLE "files" (
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+CREATE FUNCTION trigger_set_timestamp()
+RETURN TRIGGER AS $$
+BEGIN
+  NEW.updated.at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgslq;
+
+CREATE TRIGGER set_timestamp()
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp()
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();

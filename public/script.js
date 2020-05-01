5d38bb757fbde77c2eaa9e1c87fa7cee8294bf32
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-05-01 14:10:49 -0300

    user update form

--- public/script.js
+++ public/script.js
@@ -40,7 +40,7 @@ const Mask = {
     value = value.replace(/(\d{2})(\d)/, "$1.$2");
     value = value.replace(/(\d{3})(\d)/, "$1-$2");
     
-    return value
+    return value;
   }
 };
 
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,12 +1,24 @@
 const User = require('../models/User');
+const { formatCpfCnpj, formatCep } = require('../../lib/utils');
 
 module.exports = {
   registerForm(req, res) {
     return res.render('user/register');
   },
 
-  show(req, res) {
-    return res.send('Usuário cadastrado.')
+  async show(req, res) {
+    const { userId: id } = req.session;
+
+    const user = await User.findOne({ where: { id } });
+
+    if (!user) return res.render('user/register', {
+      error: 'Usuário não encontrado.'
+    });
+
+    user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj);
+    user.cep = formatCep(user.cep);
+
+    return res.render('user/index', { user });
   },
 
   async register(req, res) {
@@ -14,6 +26,6 @@ module.exports = {
 
     req.session.userId = userId;
 
-    return res.redirect('/users')
+    return res.redirect('/users');
   },
 };

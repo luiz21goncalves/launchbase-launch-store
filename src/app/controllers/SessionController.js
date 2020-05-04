60b8149481cb6f0493acffe325fc3c8db9840a85
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-05-04 15:08:45 -0300

    send email with nodemailer

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
-const User = require('../models/User');
-
 const crypto = require('crypto');
+const mailer = require('../../lib/mailer');
+
+const User = require('../models/User');
 
 module.exports = {
   loginForm(req, res) {
@@ -25,15 +26,42 @@ module.exports = {
 
   async forgot(req, res) {
     const user = req.user;
-    
-    const token = crypto.randomBytes(20).toString('hex');
 
-    let now = new Date();
-    now = now.setHours(now.getHours() + 1);
+    try {
+      const token = crypto.randomBytes(20).toString('hex');
 
-    await User.update(user.id, {
-      reset_token: token,
-      reset_token_expires: now,
-    })
+      let now = new Date();
+      now = now.setHours(now.getHours() + 1);
+
+      await User.update(user.id, {
+        reset_token: token,
+        reset_token_expires: now,
+      })
+
+      await mailer.sendMail({
+        to: user.email,
+        from: 'no-replay@launchstore.com.br',
+        subject: 'Recuperação de senha',
+        html: `
+          <h2>Perdeu a chave?</h2>
+          <p>Não se preocupe, clique no link abaixo para recuperar sua senha.</p>
+          <p>
+            <a href="${req.protocol}://localhost:3000/password-reset?token=${token}" target="_blank">
+              Recuperar senha.
+            </a>
+          </p>
+        `,
+      });
+
+      return res.render('session/forgot-password', {
+        success: "Verifique seu email para recuperar sua senha!"
+      });
+    } catch(err) {
+      console.error('SessionControler forgot', err);
+      
+      return res.render('session/forgot-password', {
+        error: 'Ocorreu um erro, tente novamente!'
+      });
+    }
   }
 }
---
+++ src/lib/mailer.js
@@ -0,0 +1,10 @@
+const nodemailer = require('nodemailer');
+
+module.exports = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "3c99ba236e9b27",
+    pass: "0a677764e6968d"
+  }
+});

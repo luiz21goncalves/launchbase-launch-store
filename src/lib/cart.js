809e4dde8542b6e0ec09eb07ee94161bd173e001
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-05-28 17:48:18 -0300

    remove item to cart

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -44,7 +44,28 @@ const Cart = {
     return this;
   },
 
-  removeOne(productId) {},
+  removeOne(productId) {
+    const inCart = this.items.find(item => item.product.id == productId);
+
+    if (!inCart)
+      return this;
+
+    inCart.quantity--;
+    inCart.price = inCart.price * inCart.quantity;
+    inCart.formattedPrice = formatPrice(inCart.price);
+
+    this.total.quantity--;
+    this.total.price -= inCart.product.price;
+    this.total.formattedPrice = formatPrice(this.total.price);
+
+    if (inCart.quantity < 1) {
+      this.items = this.items.filter(item => item.product.id != productId);
+
+      return this;
+    }
+
+    return this;
+  },
 
   delete(productId) {},
 };
